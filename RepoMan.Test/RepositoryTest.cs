badf5e01f385ec7c8e3824c38fd45c0a91b20635
Britt Ballard <davidbritton.ballard@gmail.com>
2011-09-13 12:47:09 -0500

    RepoMan v2.0

---
+++ RepoMan.Test/RepositoryTest.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace RepoMan.Test
+{
+    class RepositoryTest
+    {
+
+    }
+}
---
+++ RepoMan/Repository.cs
@@ -0,0 +1,22 @@
+﻿namespace RepoMan
+{
+    class Repository<TContext, TRepository> where TContext : IRepositoryContainer, new()
+    {
+        public TContext Context { get; set; }
+
+        public System.Linq.IQueryable<TRepository> Query()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save(TRepository entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(TRepository entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
